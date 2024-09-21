0c7d74b83148582f4c535655d0d76e7e6e731fe9
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-21 07:43:19 +0200

    initial commit

---
+++ src/main/java/com/example/servingwebcontentwithspringmvc/ServingWebContentWithSpringMVCApplication.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontentwithspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentWithSpringMVCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentWithSpringMVCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servingwebcontentwithspringmvc/ServingWebContentWithSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontentwithspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContentWithSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
